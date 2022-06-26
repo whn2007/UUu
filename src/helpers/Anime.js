58cb49f349ee63726adfb76360f07eae31eb2328
whn2007 <whn2007@nyu.edu>
2022-06-25 22:40:26 -0700

    anime finished

--- src/helpers/Anime.js
+++ src/helpers/Anime.js
@@ -5,12 +5,49 @@ import Daily from "../assets/anime/daily.png"
 import Samurai from '../assets/anime/samurai.png'
 import Cowboy from '../assets/anime/cowboy.png'
 import Jojo from '../assets/anime/jojo.png'
+import Witch from '../assets/anime/witch.png'
+import Ousama from '../assets/anime/ousama.png'
+import Spirited from '../assets/anime/spirited.png'
+import YourName from '../assets/anime/yourname.png'
+import SilentVoice from '../assets/anime/silentvoice.png'
+import Akira from '../assets/anime/akira.png'
+import Redine from '../assets/anime/redine.png'
+import Time from '../assets/anime/time.png'
+import PerfectBlue from '../assets/anime/perfectblue.png'
+import Weathering from "../assets/anime/weathering.png"
+import CowboyMovie from '../assets/anime/cowboymovie.png'
+import Mononoke from "../assets/anime/mononoke.png"
+import Paprika from '../assets/anime/paprika.png'
+import Totoro from "../assets/anime/totoro.png"
+import Castle from "../assets/anime/castle.png"
+import Kiki from '../assets/anime/kiki.png'
+import Howl from '../assets/anime/howl.png'
 
 export const AnimeList = [
+    {
+        name: 'A Silent Voice',
+        image : SilentVoice,
+        link: 'https://myanimelist.net/anime/28851/Koe_no_Katachi',   
+    },
+    {
+        name: 'Akira',
+        image : Akira,
+        link: 'https://myanimelist.net/search/all?q=akira&cat=all',   
+    },
+    {
+        name: 'Castle in the Sky',
+        image : Castle,
+        link: 'https://myanimelist.net/anime/513/Tenkuu_no_Shiro_Laputa',   
+    },
     {
         name: 'Cowboy Bebop',
         image : Cowboy,
         link: 'https://myanimelist.net/anime/1/Cowboy_Bebop?q=cowboy&cat=anime',   
+    },
+    {
+        name: 'Cowboy Bebop the Movie',
+        image : CowboyMovie,
+        link: 'https://myanimelist.net/anime/5/Cowboy_Bebop__Tengoku_no_Tobira',   
     },
     {
         name: 'Daily Lives of High School Boys',
@@ -18,14 +55,59 @@ export const AnimeList = [
         link: 'https://myanimelist.net/anime/11843/Danshi_Koukousei_no_Nichijou',   
     },
     {
+        name: 'Howl\'s Moving Castle',
+        image : Howl,
+        link: 'https://myanimelist.net/anime/431/Howl_no_Ugoku_Shiro',   
+    },
+    {
         name: 'JoJo\'s Bizarre Adventure',
         image : Jojo,
         link: 'https://myanimelist.net/anime/32182/Mob_Psycho_100?q=mob%20100&cat=anime',   
     },
     {
+        name: 'Kiki\'s Delivery Service',
+        image : Kiki,
+        link: 'https://myanimelist.net/anime/512/Majo_no_Takkyuubin',   
+    },
+    {
+        name: 'Little Witch Academia',
+        image : Witch,
+        link: 'https://myanimelist.net/anime/33489/Little_Witch_Academia_TV?q=little%20witch%20aca&cat=anime',   
+    },
+    {
         name: 'Mob Psycho 100',
         image : Mob,
         link: 'https://myanimelist.net/anime/32182/Mob_Psycho_100?q=mob%20100&cat=anime',   
+    },
+    {
+        name: 'My Neighbor Totoro',
+        image : Totoro,
+        link: 'https://myanimelist.net/anime/523/Tonari_no_Totoro?q=my%20neighbor%20totoro&cat=anime',   
+    },
+    {
+        name: 'Ousama Ranking',
+        image : Ousama,
+        link: 'https://myanimelist.net/anime/40834/Ousama_Ranking?q=ousama%20r&cat=anime',   
+    },
+    {
+        name: 'Paprika',
+        image : Paprika,
+        link: 'https://myanimelist.net/anime/1943/Paprika',   
+    },
+    {
+        name: 'Perfect Blue',
+        image : PerfectBlue,
+        link: 'https://myanimelist.net/anime/437/Perfect_Blue',   
+    },
+    {
+        name: 'Princess Mononoke',
+        image : Mononoke,
+        link: 'https://myanimelist.net/anime/164/Mononoke_Hime',   
+    },
+    {
+        name: 'Redine',
+        image : Redine,
+        link: 'https://myanimelist.net/anime/6675/Redline',   
     },
     {
         name: 'Saiki Kusuo no Ψ-nan',
@@ -41,5 +123,25 @@ export const AnimeList = [
         name: 'Samurai Champloo',
         image : Samurai,
         link: 'https://myanimelist.net/anime/205/Samurai_Champloo',   
+    },
+    {
+        name: 'Spirited Away',
+        image : Spirited,
+        link: 'https://myanimelist.net/anime/199/Sen_to_Chihiro_no_Kamikakushi',   
+    },
+    {
+        name: 'The Girl Who Leapt Through Time',
+        image : Time,
+        link: 'https://myanimelist.net/anime/2236/Toki_wo_Kakeru_Shoujo',   
+    },
+    {
+        name: 'Weathering with You',
+        image : Weathering,
+        link: 'https://myanimelist.net/anime/38826/Tenki_no_Ko',   
+    },
+    {
+        name: 'Your Name',
+        image : YourName,
+        link: 'https://myanimelist.net/manga/99314/Kimi_no_Na_wa',   
     },
 ]

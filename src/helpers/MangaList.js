6a2c86c08431715419ee706b11c3a727b36657ce
whn2007 <whn2007@nyu.edu>
2022-07-05 16:14:19 -0700

    manga

--- src/helpers/MangaList.js
+++ src/helpers/MangaList.js
@@ -25,9 +25,12 @@ import HunterxHunter from '../assets/manga/hunterxhunter.png'
 import Inuyasha from '../assets/manga/inuyasha.png'
 import KatekyoHitman from '../assets/manga/katekyohitman.png'
 import Kenji from '../assets/manga/kenji.png'
+import KimetsuNoYaiba from '../assets/manga/kimetsunoyaiba.png'
+import Kingdom from '../assets/manga/kingdom.png'
 import KoeNoKitachi from '../assets/manga/koenokitachi.png'
 import LivingALeisureLifeInAWorldOfMagic from '../assets/manga/livingaleisurelifeinaworldofmagic.png'
 import MagiSinbad from '../assets/manga/magi-sinbad.png'
+import MaginooKurimo from '../assets/manga/maginookurimono.png'
 import MinamotoKun from '../assets/manga/minamoto-kun.png'
 import MissionOfLove from '../assets/manga/missionoflove.png'
 import MousouTelepathy from '../assets/manga/mousoutelepathy.png'
@@ -36,6 +39,7 @@ import Namaikazikari from '../assets/manga/namaikazikari.png'
 import Naruto from '../assets/manga/naruto.png'
 import OldMasterQ from '../assets/manga/oldmasterq.png'
 import OnePiece from '../assets/manga/onepiece.png'
+import OnePunchMan from '../assets/manga/onepunchman.png'
 import Orange from '../assets/manga/orange.png'
 import PerfectHalf from '../assets/manga/perfecthalf.png'
 import PlatinaEnd from '../assets/manga/platinaend.png'
@@ -59,14 +63,304 @@ import Yuyuhakusho from '../assets/manga/yuyuhakusho.png'
 
 export const MangaList = [
     {
-        name: '',
-        image: ,
+        name: 'Ao Haru Ride',
+        image: AoHaruRide,
+        link: 'https://myanimelist.net/manga/24294/Ao_Haru_Ride?q=ao%20haru%20ride&cat=manga'
+    },
+    {
+        name: 'Assassination Classroom',
+        image: AssassinationClassroom,
+        link: 'https://myanimelist.net/anime/24833/Ansatsu_Kyoushitsu?q=assassination&cat=anime'
+    },
+    {
+        name: 'Bakuman',
+        image: Bakuman,
+        link: 'https://myanimelist.net/anime/7674/Bakuman?q=bakuman&cat=anime'
+    },
+    {
+        name: 'Barakamon',
+        image: Barakamon,
+        link: 'https://myanimelist.net/manga/17051/Barakamon'
+    },
+    {
+        name: 'Beelzebub',
+        image: Beezlebub,
+        link: 'https://myanimelist.net/manga/10010/Beelzebub?q=beelzebub&cat=manga'
+    },
+    {
+        name: 'Bleach',
+        image: Bleach,
+        link: 'https://myanimelist.net/manga/12/Bleach?q=bleach&cat=manga'
+    },
+    {
+        name: 'Buddha',
+        image: Buddha,
+        link: 'https://myanimelist.net/manga/1111/Buddha?q=buddha&cat=manga'
+    },
+    {
+        name: 'Death Note',
+        image: DeathNote,
+        link: 'https://myanimelist.net/manga/21/Death_Note?q=deathn%20ote&cat=manga'
+    },
+    {
+        name: 'Detective Conan',
+        image: DetectiveConan,
+        link: 'https://myanimelist.net/manga/1061/Detective_Conan?q=detective%20conan&cat=manga'
+    },
+    {
+        name: 'Domestic na Kanojo',
+        image: Domekano,
+        link: 'https://myanimelist.net/manga/70941/Domestic_na_Kanojo?q=domekano&cat=manga'
+    },
+    {
+        name: 'Doraemon',
+        image: Doraemon,
+        link: 'https://myanimelist.net/manga/1032/Doraemon?q=doraemon&cat=manga'
+    },
+    {
+        name: 'Dragonball',
+        image: Dragonball,
+        link: 'https://myanimelist.net/manga/42/Dragon_Ball?q=dragonball%20manga&cat=manga'
+    },
+    {
+        name: 'Dungeon Seeker',
+        image: DungeonSeeker,
+        link: 'https://myanimelist.net/manga/98820/Dungeon_Seeker?q=dungeon%20see&cat=manga'
+    },
+    {
+        name: 'E-Rank no Kusushi',
+        image: ERankHealer,
+        link: 'https://myanimelist.net/manga/121454/E-Rank_no_Kusushi?q=e-rank&cat=manga'
+    },
+    {
+        name: 'Ever Green',
+        image: Evergreen,
+        link: 'https://myanimelist.net/manga/30595/Ever_Green?q=evergreen&cat=manga'
+    },
+    {
+        name: 'Fairytail',
+        image: Fairytail,
+        link: 'https://myanimelist.net/manga/598/Fairy_Tail?q=fairy&cat=manga'
+    },
+    {
+        name: 'Full Metal Alchemist',
+        image: FullMetalAlchemist,
+        link: 'https://myanimelist.net/manga/25/Fullmetal_Alchemist?q=full%20metal%20alch&cat=manga'
+    },
+    {
+        name: 'Gintama',
+        image: Gintama,
+        link: 'https://myanimelist.net/manga/44/Gintama?q=gintama&cat=manga'
+    },
+    {
+        name: 'Haikyuu!!',
+        image: Haikyu,
+        link: 'https://myanimelist.net/manga/35243/Haikyuu?q=haikyu&cat=manga'
+    },
+    {
+        name: 'Half & Half',
+        image: HalfnHalf,
+        link: 'https://myanimelist.net/manga/42545/Half___Half?q=half%20%26&cat=manga'
+    },
+    {
+        name: 'Hana-kun to Koisuru Watashi',
+        image: Hanagimitokoisuruwatashi,
+        link: 'https://myanimelist.net/manga/26945/Hana-kun_to_Koisuru_Watashi?q=Hanagimito%20Koisuru&cat=manga'
+    },
+    {
+        name: 'Hibi Chouchou',
+        image: HibiChouchou,
+        link: 'https://myanimelist.net/manga/35719/Hibi_Chouchou?q=hibi%20chou&cat=manga'
+    },
+    {
+        name: 'Horimiya',
+        image: Horimiya,
+        link: 'https://myanimelist.net/manga/42451/Horimiya?q=horimiya&cat=manga'
+    },
+    {
+        name: 'Hunter x Hunter',
+        image: HunterxHunter,
+        link: 'https://myanimelist.net/manga/26/Hunter_x_Hunter?q=hunter%20x&cat=manga'
+    },
+    {
+        name: 'Inuyasha',
+        image: Inuyasha,
+        link: 'https://myanimelist.net/manga/676/InuYasha?q=inuyasha&cat=manga'
+    },
+    {
+        name: 'Katekyo Hitman Reborn!',
+        image: KatekyoHitman,
+        link: 'https://myanimelist.net/manga/47/Katekyo_Hitman_Reborn?q=katekyo%20hit&cat=manga'
+    },
+    {
+        name: 'Kenji',
+        image: Kenji,
+        link: 'https://myanimelist.net/manga/15426/Kenji?q=kenji&cat=manga'
+    },
+    {
+        name: 'Kimetsu No Yaiba',
+        image: KimetsuNoYaiba,
+        link: 'https://myanimelist.net/manga/96792/Kimetsu_no_Yaiba?q=kimetsu&cat=manga'
+    },
+    {
+        name: 'Kingdom',
+        image: Kingdom,
+        link: 'https://myanimelist.net/manga/16765/Kingdom?q=kingdom&cat=manga'
+    },
+    {
+        name: 'Koe No Kitachi',
+        image: KoeNoKitachi,
+        link: 'https://myanimelist.net/manga/56805/Koe_no_Katachi?q=koe%20no&cat=manga'
+    },
+    {
+        name: 'Magi: Sinbad no Bouken',
+        image: MagiSinbad,
+        link: 'https://myanimelist.net/manga/48025/Magi__Sinbad_no_Bouken?q=magi%20sinbad&cat=manga'
+    },
+    {
+        name: 'Magi No Okurimono',
+        image: MaginooKurimo,
+        link: 'https://myanimelist.net/manga/82041/Magi_no_Okurimono'
+    },
+    {
+        name: 'Minamoto-Kun',
+        image: MinamotoKun,
+        link: 'https://myanimelist.net/manga/30123/Minamoto-kun_Monogatari?q=minamoto&cat=manga'
+    },
+    {
+        name: 'Missions of Love',
+        image: MissionOfLove,
+        link: 'https://myanimelist.net/manga/13463/Watashi_ni_xx_Shinasai'
+    },
+    {
+        name: 'Mousou Telepathy',
+        image: MousouTelepathy,
+        link: 'https://myanimelist.net/manga/101711/Mousou_Telepathy?q=mousou%20tele&cat=manga'
+    },
+    {
+        name: 'My Hero Academy',
+        image: MyHeroAcademy,
         link: ''
     },
     {
-        name: '',
-        image: ,
-        link: ''
+        name: 'Namaikazikari',
+        image: Namaikazikari,
+        link: 'https://myanimelist.net/manga/75989/Boku_no_Hero_Academia?q=my%20hero&cat=manga'
+    },
+    {
+        name: 'Naruto',
+        image: Naruto,
+        link: 'https://myanimelist.net/manga/11/Naruto?q=naruto&cat=manga'
+    },
+    {
+        name: 'Old Master Q',
+        image: OldMasterQ,
+        link: 'https://myanimelist.net/manga/48175/Old_Master_Q?q=old%20master%20q&cat=manga'
+    },
+    {
+        name: 'One Piece',
+        image: OnePiece,
+        link: 'https://myanimelist.net/manga/13/One_Piece?q=one%20piece&cat=manga'
+    },
+    {
+        name: 'One Punch Man',
+        image: OnePunchMan,
+        link: 'https://myanimelist.net/manga/44347/One_Punch-Man?q=one%20punch%20man&cat=manga'
+    },
+    {
+        name: 'Orange',
+        image: Orange,
+        link: 'https://myanimelist.net/manga/35573/Orange'
+    },
+    {
+        name: 'Perfect Half',
+        image: PerfectHalf,
+        link: 'https://myanimelist.net/manga/131509/Perfect_Half?q=perfect%20half&cat=manga'
+    },
+    {
+        name: 'Platinum End',
+        image: PlatinaEnd,
+        link: 'https://myanimelist.net/manga/93180/Platinum_End?q=platina%20end&cat=manga'
+    },
+    {
+        name: 'Prince of Tennis',
+        image: PrinceOfTennis,
+        link: 'https://myanimelist.net/manga/49/Tennis_no_Ouji-sama?q=prince%20of%20tennis&cat=manga'
+    },
+    {
+        name: 'ReLife',
+        image: ReLife,
+        link: 'https://myanimelist.net/manga/78523/ReLIFE?q=relife&cat=manga'
+    },
+    {
+        name: 'Rurouni Kenshin',
+        image: RurouniKenshin,
+        link: 'https://myanimelist.net/manga/22/Rurouni_Kenshin__Meiji_Kenkaku_Romantan'
+    },
+    {
+        name: 'Sankarea',
+        image: Sankarea,
+        link: 'https://myanimelist.net/manga/18597/Sankarea?q=sankarea&cat=manga'
+    },
+    {
+        name: 'Shaman King',
+        image: ShamanKing,
+        link: 'https://myanimelist.net/manga/50/Shaman_King?q=shaman%20king&cat=manga'
+    },
+    {
+        name: 'Slam Dunk',
+        image: SlamDunk,
+        link: 'https://myanimelist.net/manga/51/Slam_Dunk?q=slam%20dunk&cat=manga'
+    },
+    {
+        name: 'Solo Leveling',
+        image: SoloLeveling,
+        link: 'https://myanimelist.net/manga/121496/Solo_Leveling?q=solo%20leve&cat=manga'
+    },
+    {
+        name: 'Taiyou no Ie',
+        image: Taiyounoie,
+        link: 'https://myanimelist.net/manga/26736/Taiyou_no_Ie?q=Taiyounoie&cat=manga'
+    },
+    {
+        name: 'Tasogare Otome x Amnesia',
+        image: Tasogareotomexamnesia,
+        link: 'https://myanimelist.net/manga/15805/Tasogare_Otome_x_Amnesia?q=Tasogareotomexamnesia&cat=manga'
+    },
+    {
+        name: 'The Gamer',
+        image: TheGamer,
+        link: 'https://myanimelist.net/manga/146787/The_Gamer?q=the%20gamer&cat=manga'
+    },
+    {
+        name: 'The Strongest Wizard',
+        image: TheStrongestWizard,
+        link: 'https://myanimelist.net/manga/119304/Saikyou_no_Madoushi_Hiza_ni_Ya_wo_Uketeshimatta_node_Inaka_no_Eihei_ni_Naru?q=the%20strongest%20wizard&cat=manga'
+    },
+    {
+        name: "The World's Strongest Fighter",
+        image: Thewordsstrongestfighterwhotriedtoohard,
+        link: 'https://myanimelist.net/manga/121204/Doryoku_Shisugita_Sekai_Saikyou_no_Butouka_wa_Mahou_Sekai_wo_Yoyuu_de_Ikinuku'
+    },
+    {
+        name: 'UQ Holder!',
+        image: UQHolder,
+        link: 'https://myanimelist.net/manga/57765/UQ_Holder?q=uqholder&cat=manga'
+    },
+    {
+        name: 'World Trigger',
+        image: WorldTrigger,
+        link: 'https://myanimelist.net/manga/48151/World_Trigger?q=world%20trigger&cat=manga'
+    },
+    {
+        name: 'Yasashii Sekai no Tsukurikata',
+        image: Yasashiisekainotsukurikata,
+        link: 'https://myanimelist.net/manga/18939/Yasashii_Sekai_no_Tsukurikata?q=Yasashiisekainotsukurikata&cat=manga'
+    },
+    {
+        name: 'Yuu☆Yuu☆Hakusho',
+        image: Yuyuhakusho,
+        link: 'https://myanimelist.net/manga/53/Yuu%E2%98%86Yuu%E2%98%86Hakusho?q=Yuyuhakusho&cat=manga'
     },
 
 ]
--- src/helpers/VideoGameList.js
+++ src/helpers/VideoGameList.js
@@ -1,15 +1,369 @@
-
+import ADarkRoom from '../assets/videogames/adarkroom.png'
+import ArmyOfTwo from '../assets/videogames/armyoftwo.png'
+import AssassinsCreed from '../assets/videogames/assassinscreed.png'
+import BeatSaber from '../assets/videogames/beatsaber.png'
+import Borderlands from '../assets/videogames/borderlands.png'
+import BrainAge from '../assets/videogames/brainage.png'
+import CallOfDuty from '../assets/videogames/callofduty.png'
+import Cars2 from '../assets/videogames/cars2.png'
+import Creed from '../assets/videogames/creed:risetoglory.png'
+import EliteBeatAgents from '../assets/videogames/elitebeatagents.png'
+import Fallout from '../assets/videogames/fallout.png'
+import FruitNinjaVr from '../assets/videogames/fruitninjavr.png'
+import GameOfLife from '../assets/videogames/gameoflife.png'
+import GearsOfWar from '../assets/videogames/gearsofwar.png'
+import GrandTheftAuto from '../assets/videogames/grandtheftauto.png'
+import GuitarHero from '../assets/videogames/guitarhero.png'
+import HumanResourceMachine from '../assets/videogames/humanresourcemachine.png'
+import JobSimulator from '../assets/videogames/jobsimulator.png'
+import JumpUltimateStars from '../assets/videogames/jumpultimatestars.png'
+import KingsOfGlory from '../assets/videogames/kingsofglory.png'
+import Kirby from '../assets/videogames/kirby.png'
+import Madden from '../assets/videogames/madden.png'
+import Maplestory from '../assets/videogames/maplestory.png'
+import MarioKart from '../assets/videogames/mariokart.png'
+import Megaman from '../assets/videogames/megaman.png'
+import MidnightClub from '../assets/videogames/midnightclub.png'
+import Minecraft from '../assets/videogames/minecraft.png'
+import Naruto from '../assets/videogames/naruto:ultimateninjastorm.png'
+import NBA from '../assets/videogames/nba2k.png'
+import NoMansSky from '../assets/videogames/nomanssky.png'
+import Okami from '../assets/videogames/okami.png'
+import OrbusVr from '../assets/videogames/orbusvr.png'
+import PlagueInc from '../assets/videogames/plagueinc.png'
+import PokemonGo from '../assets/videogames/pokemongo.png'
+import PokemonGold from '../assets/videogames/pokemongold.png'
+import PokemonRuby from '../assets/videogames/pokemonruby.png'
+import PokemonStadium from '../assets/videogames/pokemonstadium.png'
+import Portal from '../assets/videogames/portal.png'
+import PubG from '../assets/videogames/pubg.png'
+import RickAndMorty from '../assets/videogames/rickandmorty:virtualrick-ality.png'
+import RoadRash from '../assets/videogames/roadrash.png'
+import RocketLeague from '../assets/videogames/rocketleague.png'
+import RollerCoasterTycoon from '../assets/videogames/rollercoastertycoon.png'
+import SaintsRow from '../assets/videogames/saintsrow.png'
+import SidmeiersCivilization from '../assets/videogames/sidmeierscivilization.png'
+import SimCity from '../assets/videogames/simcity.png'
+import Skyrim from '../assets/videogames/skyrim.png'
+import SpacePirateTrainer from '../assets/videogames/spacepiratetrainer.png'
+import SuperHotVr from '../assets/videogames/superhotvr.png'
+import SuperMarioBros from '../assets/videogames/supermariobros.png'
+import SuperSmashBros from '../assets/videogames/supersmashbros.png'
+import TheJackBoxPartyPack from '../assets/videogames/thejackboxpartypack.png'
+import TheLab from '../assets/videogames/thelab.png'
+import BreathoftheWild from '../assets/videogames/thelegendofzeldabreathofthewild.png'
+import SkywardSword from '../assets/videogames/thelegendofzeldaskywardsword.png'
+import TwilightPrincess from '../assets/videogames/thelegendofzeldatwilightprincess.png'
+import TheSimpsons from '../assets/videogames/thesimpsonshit&run.png'
+import TheSims from '../assets/videogames/thesims.png'
+import TheTalosPrinciple from '../assets/videogames/thetalosprinciple.png'
+import WiisSports from '../assets/videogames/wiisports.png'
+import WormsReloaded from '../assets/videogames/wormsreloaded.png'
 
 export const VideoGameList = [
     {
-        name: '',
-        image: ,
+        name: 'A Dark Room',
+        image: ADarkRoom,
         link: ''
     },
     {
-        name: '',
-        image: ,
+        name: 'Army of Two',
+        image: ArmyOfTwo,
         link: ''
     },
-
+    {
+        name: "Assassin's Creed",
+        image: AssassinsCreed,
+        link: ''
+    },
+    {
+        name: 'Beat Saber',
+        image: BeatSaber,
+        link: ''
+    },
+    {
+        name: 'Borderlands',
+        image: Borderlands,
+        link: ''
+    },
+    {
+        name: 'Brain Age',
+        image: BrainAge,
+        link: ''
+    },
+    {
+        name: 'Call of Duty',
+        image: CallOfDuty,
+        link: ''
+    },
+    {
+        name: 'Cars 2',
+        image: Cars2,
+        link: ''
+    },
+    {
+        name: 'Creed: Rise to Glory',
+        image: Creed,
+        link: ''
+    },
+    {
+        name: 'Elite Beat Agents',
+        image: EliteBeatAgents,
+        link: ''
+    },
+    {
+        name: 'Fallout',
+        image: Fallout,
+        link: ''
+    },
+    {
+        name: 'Fruit Ninja VR',
+        image: FruitNinjaVr,
+        link: ''
+    },
+    {
+        name: 'Game of Life',
+        image: GameOfLife,
+        link: ''
+    },
+    {
+        name: 'Gears of War',
+        image: GearsOfWar,
+        link: ''
+    },
+    {
+        name: 'Grand Theft Auto',
+        image: GrandTheftAuto,
+        link: ''
+    },
+    {
+        name: 'Guitar Hero',
+        image: GuitarHero,
+        link: ''
+    },
+    {
+        name: 'Human Resource Machine',
+        image: HumanResourceMachine,
+        link: ''
+    },
+    {
+        name: 'Job Simulator',
+        image: JobSimulator,
+        link: ''
+    },
+    {
+        name: 'Jump Ultimate Stars',
+        image: JumpUltimateStars,
+        link: ''
+    },
+    {
+        name: 'Kings of Glory',
+        image: KingsOfGlory,
+        link: ''
+    },
+    {
+        name: 'Kirby',
+        image: Kirby,
+        link: ''
+    },
+    {
+        name: 'Madden',
+        image: Madden,
+        link: ''
+    },
+    {
+        name: 'Maplestory',
+        image: Maplestory,
+        link: ''
+    },
+    {
+        name: 'Mario Kart',
+        image: MarioKart,
+        link: ''
+    },
+    {
+        name: 'Megaman',
+        image: Megaman,
+        link: ''
+    },
+    {
+        name: 'Midnight Club',
+        image: MidnightClub,
+        link: ''
+    },
+    {
+        name: 'Minecraft',
+        image: Minecraft,
+        link: ''
+    },
+    {
+        name: 'Naruto: Ultimate Ninja Storm',
+        image: Naruto,
+        link: ''
+    },
+    {
+        name: 'NBA 2K',
+        image: NBA,
+        link: ''
+    },
+    {
+        name: "No Man's Sky",
+        image: NoMansSky,
+        link: ''
+    },
+    {
+        name: 'Okami',
+        image: Okami,
+        link: ''
+    },
+    {
+        name: 'OrbusVR',
+        image: OrbusVr,
+        link: ''
+    },
+    {
+        name: 'Plague Inc.',
+        image: PlagueInc,
+        link: ''
+    },
+    {
+        name: 'Pokemon Go',
+        image: PokemonGo,
+        link: ''
+    },
+    {
+        name: 'Pokemon Gold',
+        image: PokemonGold,
+        link: ''
+    },
+    {
+        name: 'Pokemon Ruby',
+        image: PokemonRuby,
+        link: ''
+    },
+    {
+        name: 'Pokemon Stadium',
+        image: PokemonStadium,
+        link: ''
+    },
+    {
+        name: 'Portal',
+        image: Portal,
+        link: ''
+    },
+    {
+        name: 'PubG',
+        image: PubG,
+        link: ''
+    },
+    {
+        name: 'Rick and Morty',
+        image: RickAndMorty,
+        link: ''
+    },
+    {
+        name: 'Road Rash',
+        image: RoadRash,
+        link: ''
+    },
+    {
+        name: 'Rocket League',
+        image: RocketLeague,
+        link: ''
+    },
+    {
+        name: 'Roller Coaster Tycoon',
+        image: RollerCoasterTycoon,
+        link: ''
+    },
+    {
+        name: "Saint's Row",
+        image: SaintsRow,
+        link: ''
+    },
+    {
+        name: "Sid Meier's Civilization",
+        image: SidmeiersCivilization,
+        link: ''
+    },
+    {
+        name: 'Sim City',
+        image: SimCity,
+        link: ''
+    },
+    {
+        name: 'Skyrim',
+        image: Skyrim,
+        link: ''
+    },
+    {
+        name: 'Space Pirate Trainer',
+        image: SpacePirateTrainer,
+        link: ''
+    },
+    {
+        name: 'Super Hot VR',
+        image: SuperHotVr,
+        link: ''
+    },
+    {
+        name: 'Super Mario Bros',
+        image: SuperMarioBros,
+        link: ''
+    },
+    {
+        name: 'Super Smash Bros',
+        image: SuperSmashBros,
+        link: ''
+    },
+    {
+        name: 'The Jack Box Party Pack',
+        image: TheJackBoxPartyPack,
+        link: ''
+    },
+    {
+        name: 'The Lab',
+        image: TheLab,
+        link: ''
+    },
+    {
+        name: 'TLoZ: Breath of the Wild',
+        image: BreathoftheWild,
+        link: ''
+    },
+    {
+        name: 'TLoZ: Skyward Sword',
+        image: SkywardSword,
+        link: ''
+    },
+    {
+        name: 'TLoZ: Twilight Princess',
+        image: TwilightPrincess,
+        link: ''
+    },
+    {
+        name: 'The Simpons Hit & Run',
+        image: TheSimpsons,
+        link: ''
+    },
+    {
+        name: 'The Sims',
+        image: TheSims,
+        link: ''
+    },
+    {
+        name: 'The Talos Principle',
+        image: TheTalosPrinciple,
+        link: ''
+    },
+    {
+        name: 'Wii Sports',
+        image: WiisSports,
+        link: ''
+    },
+    {
+        name: 'Worms Reloaded',
+        image: WormsReloaded,
+        link: ''
+    },
 ]

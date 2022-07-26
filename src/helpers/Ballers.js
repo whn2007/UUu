1a12b6c018e33d2fd022a973d732bd1decf8d4e1
whn2007 <whn2007@nyu.edu>
2022-07-26 06:16:12 +0200

    baller update

--- src/helpers/Ballers.js
+++ src/helpers/Ballers.js
@@ -4,9 +4,22 @@ import RioRoot from '../assets/ballers/rioroot.png'
 import JamesonTabor from '../assets/ballers/jamesontabor.png'
 import JoshBraun from '../assets/ballers/joshbraun.png'
 import DavidVorick from '../assets/ballers/davidvorick.png'
-import PigmentPenguin from '../assets/ballers/pigmentpenguin.png'
+import BennettHoffman from '../assets/ballers/bennethoffman.png'
+import DavidDong from '../assets/ballers/daviddong.png'
+import JarretDuran from '../assets/ballers/jarrettduran.png'
+import KibaGateaux from '../assets/ballers/kibagateaux.png'
+import NathanSegal from '../assets/ballers/nathansegal.png'
+import Eskimo from '../assets/ballers/eskimo.png'
+import PattiLi from '../assets/ballers/pattili.png'
+
+
 
 export const BallersList = [
+    {
+        name: "Bennett Hoffman",
+        image: BennettHoffman,
+        link: "https://twitter.com/bennhoffman?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+    }, 
     {
         name: "David Vorick",
         image: DavidVorick,
@@ -17,15 +30,40 @@ export const BallersList = [
         image: JamesonTabor,
         link: "https://www.jamesontabor.com/",
     }, 
+    {
+        name: "Jarrett Duran",
+        image: JarretDuran,
+        link: "https://www.instagram.com/jarrettduran/",
+    },
     // {
     //     name: "Josh Braun",
     //     image: JoshBraun,
     //     link: "https://joshbraun.com/",
-    // }, 
+    // },
+    {
+        name: 'Jordan "Eskimo" Koch',
+        image: Eskimo,
+        link: "https://twitter.com/eskimo_dev",
+    },  
     {
         name: "Kanpeki",
         image: Kanpeki,
         link: "https://www.twitch.tv/kanpeki",
+    }, 
+    {
+        name: "Kiba Gateaux",
+        image: KibaGateaux,
+        link: "https://twitter.com/kibagateaux",
+    }, 
+    {
+        name: "Nathaniel Segal",
+        image: NathanSegal,
+        link: "https://www.segalmagic.com/",
+    }, 
+    {
+        name: "Patti Li",
+        image: PattiLi,
+        link: "https://en.wikipedia.org/wiki/Hao_Zhihua",
     }, 
     {
         name: "Rio Root",
@@ -33,8 +71,13 @@ export const BallersList = [
         link: "https://www.pulserecordings.com/clients/rio-root/",
     }, 
     {
+        name: "Sifu David Dong",
+        image: DavidDong,
+        link: "https://www.goldnlion.com/about-sifu-david-dong",
+    },  
+    {
         name: "Stephen Torres",
         image: StephenTorres,
         link: "https://www.stephentorres.com/",
-    },   
+    },  
 ];

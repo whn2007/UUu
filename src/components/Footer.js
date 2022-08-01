f2ec93ba7f5e5ef5f44f8c1d598a768c1db38aaf
William Nguyen <williamnguyen@williams-mbp.lan>
2022-08-01 09:21:44 -0700

    soundcloud

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,7 @@ import RedBottomLogo from '../assets/redbottomlogo.png'
 import SpotifyIcon from '../assets/spotify.png'
 import TikTokIcon from '../assets/tiktok.png'
 import TwitchIcon from '../assets/twitch.png'
+import SoundcloudIcon from '../assets/soundcloud.png'
 import PatreonIcon from '../assets/patreon.png'
 import TwitchIconDark from '../assets/twitch2.png'
 import SpotifyIconDark from '../assets/spotify2.png'
@@ -23,24 +24,28 @@ function Footer() {
       <div className="redBottomLogo"><img src={RedBottomLogo} /></div>
       <div className="socialMedia">  
         <div className="firstHalf">
-          <a href="https://twitter.com/UUuLLC" target="_blank">
-            <TwitterIcon className="special"/>
-          </a>
-          <a id = 'tiktok' href="https://www.tiktok.com/@lordjaywu" target="_blank">
-            <img src={TikTokIcon} className = 'imported special'/>
-          </a>
           <a id = 'spotify' href="https://open.spotify.com/user/12166954044" target='_blank'>
             <img src= {SpotifyIcon} 
             className="imported"/>
           </a>
-          <a href="https://www.youtube.com/c/JayWuzer" target="_blank">
-            <YouTubeIcon />
+          <a id = 'soundcloud' href="https://soundcloud.com/jaywuzer" target='_blank'>
+            <img src= {SoundcloudIcon} 
+            className="imported"/>
+          </a>
+          <a id = 'tiktok' href="https://www.tiktok.com/@lordjaywu" target="_blank">
+            <img src={TikTokIcon} className = 'imported special'/>
+          </a>
+          <a href="https://twitter.com/UUuLLC" target="_blank">
+            <TwitterIcon className="special"/>
           </a>
           <a href="https://www.instagram.com/jaywuzer/" target="_blank">
             <InstagramIcon />
           </a>
         </div>    
         <div className="secondHalf">
+          <a href="https://www.youtube.com/c/JayWuzer" target="_blank">
+            <YouTubeIcon />
+          </a>
           <a href="https://www.linkedin.com/company/uuu-llc/" target="_blank">
             <LinkedInIcon />
           </a>
@@ -52,9 +57,6 @@ function Footer() {
           </a>
           <a href="https://www.reddit.com/user/JayWuzer/" target="_blank">
             <RedditIcon/>
-          </a>
-          <a id = 'patreon' href="https://www.patreon.com/JayWuzer" target="_blank">
-            <img src={PatreonIcon} className='imported'/>
           </a>
         </div>
       </div>

dfb30465a97e2e2d56d2cbe5b7625b12973e353d
William Nguyen <williamnguyen@Williams-MacBook-Pro.local>
2022-08-08 21:44:51 -1000

    some

--- src/helpers/ProjectsList.js
+++ src/helpers/ProjectsList.js
@@ -10,6 +10,27 @@ import StartEngine from '../assets/projects/startengine.png'
 import MeUndies from '../assets/projects/meundies.png'
 import Mous from '../assets/projects/mous.png'
 import NatureAquarium from '../assets/projects/natureaquarium.png'
+import Printful from '../assets/projects/printful.png'
+import Shopify from '../assets/projects/shopify.png'
+import OaksJewelers from '../assets/projects/oaksjewelers.png'
+import RayBan from '../assets/projects/rayban.png'
+import Jordan from '../assets/projects/jordan.png'
+import NorthFace from '../assets/projects/northface.png'
+import Apple from '../assets/projects/apple.png'
+import Tesla from '../assets/projects/tesla.png'
+import Uber from '../assets/projects/uber.png'
+import Robinhood from '../assets/projects/robinhood.png'
+import Coinbase from '../assets/projects/coinbase.png'
+import Hinge from '../assets/projects/hinge.png'
+import Mevo from '../assets/projects/mevo.png'
+import Paypal from '../assets/projects/paypal.png'
+import Kayak from '../assets/projects/kayak.png'
+import Hotels from '../assets/projects/hotels.png'
+import MuseScore from '../assets/projects/musescore.png'
+import Amazon from '../assets/projects/amazon.png'
+import Audible from '../assets/projects/audible.png'
+import Etsy from '../assets/projects/etsy.png'
+import OfferUp from '../assets/projects/offerup.png'
 
 export const ProjectsList = [  
     {
@@ -28,24 +49,79 @@ export const ProjectsList = [
         link: "https://groovelife.com/",
     }, 
     {
+        name: "Amazon",
+        image: Amazon,
+        link: "https://www.amazon.com/",
+    }, 
+    {
+        name: "Apple",
+        image: Apple,
+        link: "https://www.apple.com/",
+    }, 
+    {
+        name: "Audible",
+        image: Audible,
+        link: "https://www.audible.com/",
+    }, 
+    {
+        name: "Coinbase",
+        image: Coinbase,
+        link: "https://www.coinbase.com/",
+    }, 
+    {
         name: "Established Titles",
         image: EstablishedTitles,
         link: "https://establishedtitles.com/",
-    },     
+    },    
+    {
+        name: "Etsy",
+        image: Etsy,
+        link: "https://www.etsy.com/",
+    },  
     {
         name: "Handshake (HNS)",
         image: HNS,
         link: "https://handshake.org/",
     },   
     {
+        name: "Hinge",
+        image: Hinge,
+        link: "https://hinge.co/",
+    },  
+    {
+        name: "Hotels.com",
+        image: Hotels,
+        link: "https://www.hotels.com/",
+    },  
+    {
+        name: "Jordan",
+        image: Jordan,
+        link: "https://www.nike.com/jordan",
+    },  
+    {
+        name: "Kayak",
+        image: Kayak,
+        link: "https://www.kayak.com/",
+    }, 
+    {
         name: "MeUndies",
         image: MeUndies,
         link: "https://www.meundies.com/",
     },  
     {
+        name: "Mevo",
+        image: Mevo,
+        link: "https://www.mevo.com/",
+    },  
+    {
         name: "Mous",
         image: Mous,
         link: "https://www.mous.co/?device-picker",
+    }, 
+    {
+        name: "MuseScore",
+        image: MuseScore,
+        link: "https://musescore.org/en",
     }, 
     {
         name: "Nature Aquarium",
@@ -56,16 +132,66 @@ export const ProjectsList = [
         name: "Nemu Nemu",
         image: NemuNemu,
         link: "https://www.nemu-nemu.com/",
-    },   
+    }, 
+    {
+        name: "Oaks Jewelers",
+        image: OaksJewelers,
+        link: "https://www.oaksjewelers.com/",
+    }, 
+    {
+        name: "OfferUp",
+        image: OfferUp,
+        link: "https://offerup.com/",
+    },
+    {
+        name: "Paypal",
+        image: Paypal,
+        link: "https://www.paypal.com/us/home",
+    },  
+    {
+        name: "Printful",
+        image: Printful,
+        link: "https://www.printful.com/",
+    },  
     {
         name: "QuikFlip",
         image: Quikflip,
         link: "https://www.quikflipapparel.com/",
     },   
     {
+        name: "Ray-Ban",
+        image: RayBan,
+        link: "https://www.ray-ban.com/usa",
+    },  
+    {
+        name: "Robinhood",
+        image: Robinhood,
+        link: "https://robinhood.com/us/en/",
+    },  
+    {
+        name: "Shopify",
+        image: Shopify,
+        link: "https://www.shopify.com/",
+    },
+    {
         name: "StartEngine",
         image: StartEngine,
         link: "https://www.startengine.com/",
+    }, 
+    {
+        name: "Tesla",
+        image: Tesla,
+        link: "https://www.tesla.com/",
+    },
+    {
+        name: "The North Face",
+        image: NorthFace,
+        link: "https://www.thenorthface.com/en-us",
+    }, 
+    {
+        name: "Uber",
+        image: Uber,
+        link: "https://www.uber.com/",
     }, 
     {
         name: "Wild n Sweet Bengals",

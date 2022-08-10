e7cd51b246557d71d7a07da1d85526a034935293
William Nguyen <williamnguyen@Williams-MacBook-Pro.local>
2022-08-09 23:12:12 -1000

    some

--- src/helpers/ProjectsList.js
+++ src/helpers/ProjectsList.js
@@ -31,6 +31,42 @@ import Amazon from '../assets/projects/amazon.png'
 import Audible from '../assets/projects/audible.png'
 import Etsy from '../assets/projects/etsy.png'
 import OfferUp from '../assets/projects/offerup.png'
+import Ebay from '../assets/projects/ebay.png'
+import SeatGeek from '../assets/projects/seatgeek.png'
+import Ring from '../assets/projects/ring.png'
+import Kindle from '../assets/projects.kindle.png'
+import Google from '../assets/projects/google.png'
+import TikTok from '../assets/projects/tiktok.png'
+import Twitter from '../assets/projects/twitter.png'
+import SoundCloud from '../assets/projects/soundcloud.png'
+import Spotify from '../assets/projects/spotify.png'
+import Instagram from '../assets/projects/instagram.png'
+import Youtube from '../assets/projects/youtube.png'
+import GoogleMaps from '../assets/projects/googlemaps.png'
+import Gmail from '../assets/projects/gmail.png'
+import GSuite from '../assets/projects/gsuite.png'
+import GooglePay from '../assets/projects/googlepay.png'
+import GooglePhotos from '../assets/projects/googlephotos.png'
+import ApplePay from '../assets/projects/applepay.png'
+import WeChat from '../assets/projects/wechat.png'
+import Venmo from '../assets/projects/venmo.png'
+import Chase from '../assets/projects/chase.png'
+import OneNote from '../assets/projects/onenote.png'
+import Slack from '../assets/projects/slack.png'
+import Photoshop from '../assets/projects/photoshop.png'
+import GoogleChrome from '../assets/projects/googlechrome.png'
+import GoogleDrive from '../assets/projects/googledrive.png'
+import GooglePixel from '../assets/projects/googlepixel.png'
+import EZNameChange from '../assets/projects/eznamechange.png'
+import ProvidenceHospital from '../assets/projects/providencehospital.png'
+import NavySeals from '../assets/projects/navyseals.png'
+import FBI from '../assets/projects/fbi.png'
+import CIA from '../assets/projects/cia.png'
+import NSA from '../assets/projects/nsa.png'
+import ICann from '../assets/projects/icann.png'
+import OnePlus from '../assets/projects/oneplus.png'
+import Vaniply from '../assets/projects/vaniply.png'
+import WholeFoods from '../assets/projects/wholefoods.png'
 
 export const ProjectsList = [  
     {
@@ -59,15 +95,35 @@ export const ProjectsList = [
         link: "https://www.apple.com/",
     }, 
     {
+        name: "Apple Pay",
+        image: ApplePay,
+        link: "https://www.apple.com/apple-pay/",
+    }, 
+    {
         name: "Audible",
         image: Audible,
         link: "https://www.audible.com/",
+    }, 
+    {
+        name: "Chase",
+        image: Chase,
+        link: "https://www.chase.com/",
+    }, 
+    {
+        name: "CIA",
+        image: CIA,
+        link: "https://www.cia.gov/",
     }, 
     {
         name: "Coinbase",
         image: Coinbase,
         link: "https://www.coinbase.com/",
     }, 
+    {
+        name: "Ebay",
+        image: Ebay,
+        link: "https://www.ebay.com/",
+    },  
     {
         name: "Established Titles",
         image: EstablishedTitles,
@@ -77,6 +133,61 @@ export const ProjectsList = [
         name: "Etsy",
         image: Etsy,
         link: "https://www.etsy.com/",
+    },  
+    {
+        name: "EZ Name Change",
+        image: EZNameChange,
+        link: "https://eznamechange.com/",
+    },
+    {
+        name: "FBI",
+        image: FBI,
+        link: "https://www.fbi.gov/contact-us",
+    }, 
+    {
+        name: "Gmail",
+        image: Gmail,
+        link: "https://mail.google.com/",
+    },  
+    {
+        name: "Google",
+        image: Google,
+        link: "https://www.google.com/",
+    },  
+    {
+        name: "Google Chrome",
+        image: GoogleChrome,
+        link: "https://www.google.com/chrome/",
+    },  
+    {
+        name: "Google Drive",
+        image: GoogleDrive,
+        link: "https://www.google.com/drive/",
+    },
+    {
+        name: "Google Maps",
+        image: GoogleMaps,
+        link: "https://www.google.com/maps",
+    },  
+    {
+        name: "Google Pay",
+        image: GooglePay,
+        link: "https://pay.google.com/",
+    },  
+    {
+        name: "Google Photos",
+        image: GooglePhotos,
+        link: "https://www.google.com/photos/about/",
+    },  
+    {
+        name: "Google Pixel",
+        image: GooglePixel,
+        link: "https://store.google.com/us/category/phones?pli=1&hl=en-US",
+    },  
+    {
+        name: "GSuite",
+        image: GSuite,
+        link: "https://workspace.google.com/",
     },  
     {
         name: "Handshake (HNS)",
@@ -92,6 +203,16 @@ export const ProjectsList = [
         name: "Hotels.com",
         image: Hotels,
         link: "https://www.hotels.com/",
+    }, 
+    {
+        name: "ICANN",
+        image: ICann,
+        link: "https://www.icann.org/",
+    }, 
+    {
+        name: "Instagram",
+        image: Instagram,
+        link: "https://www.instagram.com/",
     },  
     {
         name: "Jordan",
@@ -102,6 +223,11 @@ export const ProjectsList = [
         name: "Kayak",
         image: Kayak,
         link: "https://www.kayak.com/",
+    }, 
+    {
+        name: "Kindle",
+        image: Kindle,
+        link: "https://www.amazon.com/Kindle-Now-with-Built-in-Front-Light/dp/B07978J597/ref=sr_1_1?crid=21G38MUN6REAI&keywords=kindle&qid=1660068263&s=amazon-devices&sprefix=ki%2Camazon-devices%2C224&sr=1-1",
     }, 
     {
         name: "MeUndies",
@@ -129,9 +255,19 @@ export const ProjectsList = [
         link: "https://www.instagram.com/natureaquarium/?hl=en",
     }, 
     {
+        name: "Navy Seals",
+        image: NavySeals,
+        link: "https://navyseals.com/",
+    }, 
+    {
         name: "Nemu Nemu",
         image: NemuNemu,
         link: "https://www.nemu-nemu.com/",
+    }, 
+    {
+        name: "NSA",
+        image: NSA,
+        link: "https://www.nsa.gov/",
     }, 
     {
         name: "Oaks Jewelers",
@@ -144,14 +280,34 @@ export const ProjectsList = [
         link: "https://offerup.com/",
     },
     {
+        name: "OneNote",
+        image: OneNote,
+        link: "https://www.onenote.com/notebooks",
+    },
+    {
+        name: "OnePlus",
+        image: OnePlus,
+        link: "https://www.oneplus.com/",
+    },
+    {
         name: "Paypal",
         image: Paypal,
         link: "https://www.paypal.com/us/home",
     },  
     {
+        name: "Photoshop",
+        image: Photoshop,
+        link: "https://www.adobe.com/products/photoshop.html",
+    },  
+    {
         name: "Printful",
         image: Printful,
         link: "https://www.printful.com/",
+    },  
+    {
+        name: "Providence Hospital",
+        image: ProvidenceHospital,
+        link: "https://www.providence.org/",
     },  
     {
         name: "QuikFlip",
@@ -164,14 +320,39 @@ export const ProjectsList = [
         link: "https://www.ray-ban.com/usa",
     },  
     {
+        name: "Ring",
+        image: Ring,
+        link: "https://ring.com/",
+    }, 
+    {
         name: "Robinhood",
         image: Robinhood,
         link: "https://robinhood.com/us/en/",
     },  
     {
+        name: "SeatGeek",
+        image: SeatGeek,
+        link: "https://seatgeek.com/",
+    },
+    {
         name: "Shopify",
         image: Shopify,
         link: "https://www.shopify.com/",
+    },
+    {
+        name: "Slack",
+        image: Slack,
+        link: "https://slack.com/",
+    },
+    {
+        name: "Soundcloud",
+        image: SoundCloud,
+        link: "https://soundcloud.com/",
+    },
+    {
+        name: "Spotify",
+        image: Spotify,
+        link: "https://www.spotify.com/us/",
     },
     {
         name: "StartEngine",
@@ -189,13 +370,48 @@ export const ProjectsList = [
         link: "https://www.thenorthface.com/en-us",
     }, 
     {
+        name: "TikTok",
+        image: TikTok,
+        link: "https://www.tiktok.com/en/",
+    },
+    {
+        name: "Twitter",
+        image: Twitter,
+        link: "https://twitter.com/",
+    },
+    {
         name: "Uber",
         image: Uber,
         link: "https://www.uber.com/",
     }, 
     {
+        name: "Vaniply",
+        image: Vaniply,
+        link: "https://www.amazon.com/Vaniply-Ointment-Tube-dermatologist-preservative/dp/B004Q1B5CG",
+    }, 
+    {
+        name: "Venmo",
+        image: Venmo,
+        link: "https://venmo.com/",
+    }, 
+    {
+        name: "WeChat",
+        image: WeChat,
+        link: "https://www.wechat.com/",
+    }, 
+    {
+        name: "Whole Foods",
+        image: WholeFoods,
+        link: "https://www.wholefoodsmarket.com/",
+    },   
+    {
         name: "Wild n Sweet Bengals",
         image: Bengals,
         link: "https://wildnsweetbengals.com/en/",
-    },       
+    },   
+    {
+        name: "Youtube",
+        image: Youtube,
+        link: "https://www.youtube.com/",
+    },    
 ];

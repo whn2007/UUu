8ae3e7ac76346e0fc7df757ecde7f8600aa6fe96
William Nguyen <williamnguyen@Williams-MacBook-Pro.local>
2022-08-10 09:55:37 -1000

    villains

--- src/helpers/VillainList.js
+++ src/helpers/VillainList.js
@@ -1,94 +1,93 @@
-
+import RaymondGhermezian from '../assets/villains/raymond.png'
+import RichardKirkendall from '../assets/villains/richardkirkendall.png'
+import EugeneWu from '../assets/villains/eugenewu.png'
+import TashiWang from '../assets/villains/tashiwang.png'
+import TieshunRoquerre from '../assets/villains/tieshunroquerre.png'
+import AnthonyLiu from '../assets/villains/anthonyliu.png'
+import JohnFaverot from '../assets/villains/johnfaverot.png'
+import LydiaGu from '../assets/villains/lydiagu.png'
+import CraigJohnson from '../assets/villains/craigjohnson.png'
+import DavidCowles from '../assets/villains/davidcowles.png'
+import FelixBennett from '../assets/villains/felixbennett.png'
+import Placeholder from '../assets/ballers/placeholder.png'
+import MarkSmith from '../assets/villains/marksmith.png'
+import RyoMiller from '../assets/villains/ryomiller.png'
+import NatalieJohnson from '../assets/villains/nataliejohnson.png'
 
 export const VillainList = [
-    // {
-    //     name: 'Craig Johnson',
-    //     image: CraigJohnson,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Tieshun Roquerre',
-    //     image: TieshunRoquerre,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Raymond Ghermezian',
-    //     image: RaymondGhermezian,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Anthony Liu',
-    //     image: AnthonyLiu,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Richard Kirkendall',
-    //     image: RichardKirkendall,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Hillan Klein',
-    //     image: HillanKlein,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Richard Kirkendall',
-    //     image: RichardKirkendall,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Jenn Suarez',
-    //     image: JennSuarez,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Natalie Johnson',
-    //     image: NatalieJohnson,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Kai Miller',
-    //     image: KaiMiller,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Ryo Miller',
-    //     image: RyoMiller,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Jake Schaeffer',
-    //     image: JakeSchaeffer,
-    //     link: ''
-    // },
-    // {
-    //     name: 'John Faverot',
-    //     image: JohnFaverot,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Kos Grilles',
-    //     image: KosGrilles,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Liz Kim',
-    //     image: LizKim,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Tiger Shen',
-    //     image: TigerShen,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Phil Shen',
-    //     image: PhilShen,
-    //     link: ''
-    // },
-    // {
-    //     name: 'Mark Smith',
-    //     image: MarkSmith,
-    //     link: ''
-    // },
+    {
+        name: 'Raymond Ghermezian',
+        image: RaymondGhermezian,
+        link: 'https://www.yelp.com/biz/raymond-ghermezian-a-professional-law-corp-los-angeles?hrid=nPhUKDmzABTbhjmBwck2sw&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)'
+    },
+    {
+        name: 'Richard Kirkendall',
+        image: RichardKirkendall,
+        link: 'https://twitter.com/NamecheapCEO'
+    },
+    {
+        name: 'Tieshun Roquerre',
+        image: TieshunRoquerre,
+        link: 'https://tieshunroquerre.com/'
+    },
+    {
+        name: 'Anthony Liu',
+        image: AnthonyLiu,
+        link: 'https://twitter.com/imigliu'
+    },
+    {
+        name: 'Craig Johnson',
+        image: CraigJohnson,
+        link: 'https://www.linkedin.com/in/craigjson/'
+    },
+    {
+        name: 'David Cowles',
+        image: DavidCowles,
+        link: 'https://twitter.com/RunDavidMC'
+    },
+    {
+        name: 'Eugene Wu',
+        image: EugeneWu,
+        link: 'http://www.cs.columbia.edu/~ewu/'
+    },
+    {
+        name: 'Felix Bennett',
+        image: FelixBennett,
+        link: 'https://twitter.com/felixbennett_'
+    },
+    {
+        name: 'John Faverot',
+        image: JohnFaverot,
+        link: 'https://www.linkedin.com/in/john-xu-software'
+    },
+    {
+        name: 'Kai Miller',
+        image: Placeholder,
+        link: 'https://www.linkedin.com/in/kai-miller-037a38b6/'
+    },
+    {
+        name: 'Lydia Gu',
+        image: LydiaGu,
+        link: 'https://www.linkedin.com/in/lydiagu'
+    },
+    {
+        name: 'Mark Smith',
+        image: MarkSmith,
+        link: 'https://www.linkedin.com/in/namesakemark'
+    },
+    {
+        name: 'Ryo Miller',
+        image: RyoMiller,
+        link: 'https://www.linkedin.com/in/ryo-miller-1766b5169/'
+    },
+    {
+        name: 'Natalie Johnson',
+        image: NatalieJohnson,
+        link: 'https://www.linkedin.com/in/natalie-vc-johnson/'
+    },
+    {
+        name: 'Tashi Wang',
+        image: TashiWang,
+        link: 'https://www.linkedin.com/in/tashi-wang-ba17a01b0'
+    },
 ]
--- src/pages/Shoutouts.js
+++ src/pages/Shoutouts.js
@@ -245,7 +245,7 @@ function Shoutouts() {
             <p>Become a <a href='https://merch.uuu.vip/collections/uuu-merch' target='none'>patron</a> of UUu LLC to be forgiven</p>
         </div>
 
-        {/* <div className="aboutList">
+        <div className="aboutList">
           {VillainList.map((aboutItem, key) => {
             return (
               <ShoutOutItem
@@ -257,7 +257,7 @@ function Shoutouts() {
             );
           })}
         </div>
-         */}
+        
 
         <Footer className = "footer"/>
     </div>
